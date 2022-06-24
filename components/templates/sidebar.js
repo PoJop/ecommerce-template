d87d2cc52734e970612e06b94460ecc0f4e3999f
PoJop <kostetzkiy.oleg@gmail.com>
2022-06-24 21:17:00 +0300

    The trick to viewing units of measure on mobile devices

--- components/templates/sidebar.js
+++ components/templates/sidebar.js
@@ -23,7 +23,7 @@ export const Sidebar = ({ show, close, position = 'left', children, header = '' 
                 <>
                     <aside style={{ transform: `translateX(${!show ? pos : 0})` }} className={`fixed ${position === "left" ? "left-0" : "right-0"}  top-0 z-[1000] h-screen transition duration-150 ease-out`}>
 
-                        <div className="flex flex-col h-screen bg-white drop-shadow-3xl w-[100vw] max-w-[270px] lg:max-w-[350px]  ">
+                        <div className="flex flex-col custom_100vh bg-white drop-shadow-3xl w-[100vw] max-w-[270px] lg:max-w-[350px]  ">
 
                             <header className={`flex justify-between px-4 py-3 border-b border-dull-gray `}>
                                 <h3 className="text-2xl text-gray-900">{header}</h3>
@@ -34,7 +34,7 @@ export const Sidebar = ({ show, close, position = 'left', children, header = '' 
                     </aside>
                     <aside
                         onClick={() => close(false)}
-                        className={`fixed ${!show && "hidden"}  z-[999] blur-sm top-0 left-0 w-screen h-screen bg-[rgba(0,0,0,.1)] transition duration-250 ${show ? "opacity-100" : "opacity-0"}`}
+                        className={`fixed ${!show && "hidden"}  z-[999] blur-sm top-0 left-0 w-screen custom_100vh bg-[rgba(0,0,0,.1)] transition duration-250 ${show ? "opacity-100" : "opacity-0"}`}
                     />
                 </>
             }
--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,23 @@ import "slick-carousel/slick/slick-theme.css";
 import { CartProvider } from '../contextes/cart-conrext';
 import Head from 'next/head';
 import { CatalogProvider } from '../contextes/catalog-context';
+import React from 'react';
 
 function MyApp({ Component, pageProps }) {
+
+  React.useEffect(() => {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    // We listen to the resize event
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  }, [])
   return (
     <>
       <CatalogProvider>
